9d1ba9094c5ab6bffe714b1b3566d996c6aedcc9
Fasihagul <sughrabano82@gmail.com>
2024-10-21 20:30:35 +0500

    Add files via upload

---
+++ New folder (4)/app.js
@@ -0,0 +1,22 @@
+var input=document.getElementById("input")//global variable
+
+function getValue(number) {
+    input.value += number  ;
+  }
+  
+  function clearall() {
+    input.value = " ";
+  }
+
+  function deleteCh() {
+    input.value = input.value.slice(0, -1);
+  }
+  
+  function square() {
+    input.value = input.value * input.value;
+  }
+  
+  function answer() {
+      input.value = eval(input.value);
+    }
+  
